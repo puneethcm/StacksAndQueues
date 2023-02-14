0bf62900c1137040e3198da45ee1c541ebd91d9e
puneethcm <puneethkumarcm8050@gmail.com>
2023-02-14 13:14:30 +0530

    Enqueues'

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -10,15 +10,29 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("\nWelcome to Stacks And Queues");
-        Stack stack = new Stack();
-        stack.Push(70);
-        stack.Push(30);
-        stack.Push(56);
-        stack.Display();
-        stack.Peek();
-        stack.Pop();
-        //stack.Display();
-        stack.isEmpty();
+        Console.WriteLine("\n1: Stacks Operations\n2:Queues Operations");
+        int option = Convert.ToInt32(Console.ReadLine());
+        switch (option)
+        {
+            case 1:
+                Stack stack = new Stack();
+                stack.Push(70);
+                stack.Push(30);
+                stack.Push(56);
+                stack.Display();
+                stack.Peek();
+                stack.Pop();
+                stack.isEmpty();
+                break;
+
+            case 2:
+                Queues queues = new Queues();
+                queues.Enqueue(56);
+                queues.Enqueue(30);
+                queues.Enqueue(70);
+                queues.Display();
+                break;
+        }
         Console.ReadLine();
     }
 }
---
+++ StacksAndQueues/Queues.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace StacksAndQueues
+{
+	public class Queues
+	{
+		public Node head;
+
+		public void Enqueue(int data)
+		{
+			Node new_node = new Node(data);
+			if(head == null)
+			{
+				head = new_node;
+			}
+			else
+			{
+				Node temp = head;
+				while (temp.next != null)
+				{
+					temp = temp.next;
+				}
+				temp.next = new_node;
+			}
+		}
+        public void Display()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("\nStack is Empty");
+            }
+            else
+            {
+                Console.WriteLine("\nDisplaying the Nodes");
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
+
